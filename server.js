fffeb8ad912344d1fd9d9c930a638ad3ee528803
jiho1996 <jiho.urssu@gmail.com>
2022-09-05 19:02:32 +0900

    feat : id에 따른 상품 조회 및 post 코드 개선

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const exress = require('express');
+
 const cors = require('cors');
 const express = require('express');
 const app = express();
+const models = require('./models')
 const port = 8080;
 
 app.use(express.json());
@@ -10,52 +11,72 @@ app.use(cors());
 app.get("/products/:id", (req,res)=>{
   const params = req.params;
   const {id} = params;
-  res.send(`id는 ${id}`)
-})
+  models.Product.findOne({
+    where : {
+      id
+    },
+  })
+  .then((result) => {
+    console.log(`product : ${result}`);
+    res.send({
+      product : result,
+    });
+  })
+  .catch((err) => {
+    console.error(err)
+    res.send("상품 조회에 에러가 발생.")
+  })
+});
 
 
 app.get("/products",(req,res) => {
-  const params = req.params;
-  const {id} = params;
+  models.Product.findAll({
+    //limit : 1,
+    order : [["createdAt", "DESC"]],
+    attributes : ["id", "name", "price", "createdAt", "seller", "imageUrl"],
+  }).then((result) => {
+    
     res.send({
-        products:[
-            {
-                
-                    "id" : 1,
-                  "name": "샤넬 가방",
-                  "price": 900000,
-                  "seller": "샤넬",
-                  "imgUrl": "images/chanel.jpg"
-                },
-                {
-                    "id" : 2,
-                  "name": "루이비통 가방",
-                  "price": 5000000,
-                  "seller": "루이비통",
-                  "imgUrl": "images/louisVuitton.jpeg"
-                },
-                {
-                    "id" : 3,
-                  "name": "구찌 가방",
-                  "price": 7000000,
-                  "seller": "구찌",
-                  "imgUrl": "images/Gucci.jpeg"
-                },
-        ],
-    });
-        
-    
-    
+      products : result
+    })
+  })
+  .catch((err) => {
+    res.send(`에러 발생 ${err}`)
+  })
 });
 
 app.post("/products", (req,res)=>{
   const body = req.body;
-  console.log(body)
-  res.send({
-    body,
-  });
+  const {name, description, price, seller} = body;
+  if (!name || !description || !price || !seller){
+    res.send("모든 필드를 입력하세요.")
+  }
+  models.Product.create({
+    name,
+    description,
+    price,
+    seller
+  })
+  .then((result) =>{
+    console.log(`상품 생성 ${result}`)
+    res.send({
+      result,
+    });
+  }).catch((err) => {
+    console.error(err);
+    res.send("상품 업로드 에러", err)
+  })
 });
 
 app.listen(port, () =>{
-    console.log("서버가 돌아가고 있습니다.")
+    console.log("서버가 돌아가고 있습니다.");
+    models.sequelize.sync().then(()=>{
+      console.log('db 연결 성공!');
+    })
+    .catch((err) =>{
+      console.error(err);
+      console.log("db 연결 에러ㅜ");
+      process.exit();
+    
+})
 })
